5fc7eb2ba10e0ef65cf96f75f847eaabb6d8daf7
Madeline Whitmore <madeline-whitmore@proton.me>
2023-11-14 13:00:18 +1300

    ✨ More Vanilla Like Functionality Bring bonemeal functionality more inline with pink petals. Added mirroring and rotation methods from AbstractBlock to flowers via mixin.

--- src/main/java/io/github/ItsMaddieNow/building_tweaks/mixin/FlowerBlockMixin.java
+++ src/main/java/io/github/ItsMaddieNow/building_tweaks/mixin/FlowerBlockMixin.java
@@ -3,7 +3,10 @@ package io.github.ItsMaddieNow.building_tweaks.mixin;
 import io.github.ItsMaddieNow.building_tweaks.flowers.FlowerTweaks;
 import net.minecraft.block.*;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.World;
@@ -30,19 +33,40 @@ public class FlowerBlockMixin extends Block implements Fertilizable {
 		world.setBlockState(pos,state.with(FlowerTweaks.FLOWERS,i),2);
 	}
 	public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient){
-		return (getFlowers(state) < FlowerTweaks.MAX_FLOWERS && FlowerTweaks.allowedFlower(state.getBlock()) && !state.isIn(FlowerTweaks.NO_BONEMEAL));
+		return (FlowerTweaks.allowedFlower(state.getBlock()) && !state.isIn(FlowerTweaks.NO_BONEMEAL));
 	}
-	public boolean canGrow(World world, RandomGenerator random, BlockPos pos, BlockState state) { return true; }
-	public void grow(ServerWorld world, RandomGenerator random, BlockPos pos, BlockState state) {
-		this.applyFlowers(world,pos,state);
+
+	@Override
+	public boolean canFertilize(World world, RandomGenerator random, BlockPos pos, BlockState state) {
+		return true;
 	}
+
+	@Override
+	public void fertilize(ServerWorld world, RandomGenerator random, BlockPos pos, BlockState state) {
+		if (getFlowers(state) < FlowerTweaks.MAX_FLOWERS){
+			this.applyFlowers(world,pos,state);
+		} else {
+			dropStack(world, pos, new ItemStack(this));
+		}
+	}
+
 	@Override
 	public boolean canReplace(BlockState state, ItemPlacementContext context){
-		return !context.shouldCancelInteraction() && context.getStack().isOf(this.asItem()) && (Integer) state.get(FlowerTweaks.FLOWERS) < FlowerTweaks.MAX_FLOWERS ? true : super.canReplace(state, context);
+		return !context.shouldCancelInteraction() && context.getStack().isOf(this.asItem()) && state.get(FlowerTweaks.FLOWERS) < FlowerTweaks.MAX_FLOWERS || super.canReplace(state, context);
 	}
+
 	@Override
+	public BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.rotate(mirror.getRotation(state.get(FlowerTweaks.FACING)));
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, BlockRotation rotation){
+		return state.with(FlowerTweaks.FACING, rotation.rotate(state.get(FlowerTweaks.FACING)));
+	}
+
 	public BlockState getPlacementState(ItemPlacementContext ctx){
 		BlockState blockState = ctx.getWorld().getBlockState(ctx.getBlockPos());
-		return blockState.isOf(this)? (BlockState) blockState.with(FlowerTweaks.FLOWERS, Math.min(FlowerTweaks.MAX_FLOWERS, (Integer)blockState.get(FlowerTweaks.FLOWERS)+1)):(BlockState)this.getDefaultState().with(FlowerTweaks.FACING, ctx.getPlayerFacing().getOpposite());
+		return blockState.isOf(this)?  blockState.with(FlowerTweaks.FLOWERS, Math.min(FlowerTweaks.MAX_FLOWERS, blockState.get(FlowerTweaks.FLOWERS)+1)):this.getDefaultState().with(FlowerTweaks.FACING, ctx.getPlayerFacing().getOpposite());
 	}
 }
