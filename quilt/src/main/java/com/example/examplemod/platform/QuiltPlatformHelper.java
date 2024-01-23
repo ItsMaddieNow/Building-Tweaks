b5f5b58f79616f0345e789dc16c5f41bc3c1622b
Madeline Whitmore <madeline-whitmore@proton.me>
2024-01-23 13:28:23 +1300

    Downgrade to 1.20.1 and add Quilt Support Deactivate Fabric Support

---
+++ quilt/src/main/java/com/example/examplemod/platform/QuiltPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.example.examplemod.platform;
+
+import com.example.examplemod.platform.services.IPlatformHelper;
+import org.quiltmc.loader.api.QuiltLoader;
+
+public class QuiltPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return QuiltLoader.isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return QuiltLoader.isDevelopmentEnvironment();
+    }
+}
